8d74fc449bd88eaded4161b318b7ad8dbd4e2290
mac <15711367521@163.com>
2019-09-09 21:37:55 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ function component() {
     return true;
 }
 component()
+(function (name, context, definition) {
+  'use strict';
+  /* istanbul ignore next */
+  if (typeof define === 'function' && define.amd) {
+    define(definition);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = definition();
+  } else {
+    context[name] = definition(name, context);
+  }
+}('PubSub', this, function (name, context) {
+  let PubSub = null;
+  return PubSub;
+}));
